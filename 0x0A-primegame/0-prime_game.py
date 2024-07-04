2cb393330eb125ade6e167ebbf014443b5917ea8
BnKeTia <bernardnketia51@gmail.com>
2024-07-04 10:23:49 +0000

    prime game refix code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,60 +1,42 @@
 #!/usr/bin/python3
-def iswinner(x, nums):
-    def sieve(n):
-        """
-        Generate a list of primes up to nusing the sieve of Eratosthene
-        """
-        is_prime = [True] * (n + 1)
-        p = 2
-        while (p * p <= n):
-            if is_prime[p]:
-                for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
+"""Module to solve Prime Game using Sieve of Eratosthenes Algorithm"""
 
-            p += 1
-        primes = [p for p in range(2, n + 1) if is_prime[p]]
-        return primes
+
+def isWinner(x, nums):
+    """Function to determine winner of Prime Game"""
 
     if not nums or x < 1:
         return None
 
-    max_n = max(nums)
-    primes = sieve(max_n)
+    max_limit = max(nums)
+    is_prime = [True for _ in range(max(max_limit + 1, 2))]
 
-    def play_game(n):
-        available_numbers = list(range(1, n + 1))
-        prime_set = set(primes)
-        current_turn = 0 # 0 for maria 1 for Ben
+    for i in range(2, int(pow(max_limit, 0.5)) + 1):
+        if not is_prime[i]:
+            continue
 
-        while True:
-            made_move = False
-            for p in primes:
-                if p > n:
-                    break
-                if p in available_numbers:
-                    # Remove p and its multiples
-                    available_numbers = [num for num in available_numbers if num % p !=0]
-                    made_move = True
-                    current_turn = 1 - current_turn # switch turns
-                break
-            if not made_move:
-                break
+        for j in range(i*i, max_limit + 1, i):
+            is_prime[j] = False
 
-        return "Maria" if current_turn == 1 else "Ben"
+    is_prime[0] = is_prime[1] = False
+    cumulative_primes = 0
 
-    maria_wins = 0
-    ben_wins = 0
+    for i in range(len(is_prime)):
+        if is_prime[i]:
+            cumulative_primes += 1
+        is_prime[i] = cumulative_primes
 
-    for num in nums:
-        winner = play_games(num)
-        if winner == "Maria":
-            maria_wins += 1
-        else:
-            ben_wins += 1
+    winner = ''
+    score = 0
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
+    for n in nums:
+        score += is_prime[n] % 2 == 1
+
+    if score * 2 == len(nums):
+        winner = None
+    if score * 2 > len(nums):
+        winner = "Maria"
     else:
-        return None
+        winner = "Ben"
+
+    return winner
